fb43dee00e8168f6358525a87d86c2721148f3d6
Pat <patrick.edwards@biarri.com>
2019-06-21 22:01:14 -0700

    More pinchers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './mr_pinchers_1.jpg';
+import mr_pinchers_1 from './mr_pinchers_1.jpg';
+import mr_pinchers_2 from './mr_pinchers_2.jpg';
+import mr_pinchers_3 from './mr_pinchers_3.jpg';
+import mr_pinchers_4 from './mr_pinchers_4.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="row">
+          <img src={mr_pinchers_1} className="App-logo" alt="logo" />
+          <img src={mr_pinchers_2} className="App-logo" alt="logo" />
+        </div>
+        <div className="row">
+          <img src={mr_pinchers_3} className="App-logo" alt="logo" />
+          <img src={mr_pinchers_4} className="App-logo" alt="logo" />
+        </div>
       </header>
     </div>
   );
